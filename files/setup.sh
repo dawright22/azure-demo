befd66bbeb902e91a69263ad555021c55b59cd13
dawright22 <53329137+dawright22@users.noreply.github.com>
2022-05-23 17:28:24 +1000

    Update setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -4,7 +4,7 @@
 # cd /tmp
 sudo apt-get -y update > /dev/null 2>&1
 sudo apt install -y unzip jq cowsay mysql-client > /dev/null 2>&1
-wget https://releases.hashicorp.com/vault//1.10.3/vault_1.10.3_linux_amd64.zip
+wget https://releases.hashicorp.com/vault/1.10.3/vault_1.10.3_linux_amd64.zip
 sudo unzip vault_1.1.1_linux_amd64.zip -d /usr/local/bin/
 
 # Set Vault up as a systemd service
