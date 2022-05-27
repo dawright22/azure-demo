1426ca3172f44ee89d0f2691f14efa618213b5a2
dawright22 <david@wrightsonline.info>
2022-05-27 10:57:33 +1000

    f11

--- main.tf
+++ main.tf
@@ -262,6 +262,7 @@ resource "azurerm_mysql_server" "mysql" {
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
   ssl_enforcement_enabled           = false
+  ssl_minimal_tls_version_enforced  = "TLSEnforcementDisabled"
 }
 
 /* This is a sample database that we'll populate with data from our app.
